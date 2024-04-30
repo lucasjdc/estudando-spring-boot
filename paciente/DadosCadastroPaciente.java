7a8404431aebc87b2e20aebd2031a5a159281911
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-04-30 14:52:51 -0300

    Cadastro de pacientes

---
+++ paciente/DadosCadastroPaciente.java
@@ -0,0 +1,24 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.Valid;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        String cpf,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
+}
