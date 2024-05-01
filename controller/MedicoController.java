9ec234eb7c308db59f9b7e91db46c7873b5cf789
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-05-01 01:14:39 -0300

    Exclusão lógica

--- controller/MedicoController.java
+++ controller/MedicoController.java
@@ -24,7 +24,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort= {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -32,12 +32,13 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         Medico medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
-
     }
+
     @DeleteMapping("/{id}")
     @Transactional
     public void excluir(@PathVariable Long id) {
-        repository.deleteById(id);
+        Medico medico = repository.getReferenceById(id);
+        medico.excluir();
 
     }
 }
