40fe73136af4cfb6aa73f4440d8f82a861cb1ae5
lucasjdc <lucasjpdc@gmail.com>
2024-11-22 21:27:25 -0300

    Requisições DELETE

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -34,4 +34,10 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
 
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
